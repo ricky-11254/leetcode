5edebb0ca643cfc0e38abbcc6a4aa5fa61dfb6e0
ricky <erickmuchiri11@gmail.com>
2024-04-10 12:10:07 +0300

    commit

---
+++ 14.LongestCommonPrefix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs == null || strs.Length == 0)
+        {
+            return "";
+        }
+
+        string prefix = strs[0];
+        for (int i = 1; i < strs.Length; i++)
+        {
+            while (strs[i].IndexOf(prefix) != 0)
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (prefix.Length == 0)
+                {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string[] strs = Console.ReadLine().Split(' ');
+        string result = solution.LongestCommonPrefix(strs);
+        Console.WriteLine(result);
+    }
+}
